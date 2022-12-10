080286a4327c76abb94b68bb84c3a08e0c03b3dd
Benito Pedro <benitopedro21@outlook.com>
2022-12-10 15:15:07 -0300

    Show `Limit Order` link on `Swap Modal`

--- src/components/swap/SwapHeader.tsx
+++ src/components/swap/SwapHeader.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import styled from 'styled-components'
 
 import { TYPE } from '../../theme'
+import { StyledNavLink } from '../Header'
 import { RowBetween } from '../Row'
 import Settings from '../Settings'
 
@@ -11,6 +12,10 @@ const StyledSwapHeader = styled.div`
   width: 100%;
   max-width: 420px;
   color: ${({ theme }) => theme.text2};
+`
+const StyledFlexDiv = styled.div`
+  display: flex;
+  align-items: center;
 `
 
 export default function SwapHeader({
@@ -23,9 +28,26 @@ export default function SwapHeader({
   return (
     <StyledSwapHeader>
       <RowBetween>
-        <TYPE.black my={2} fontWeight={500}>
-          {title}
-        </TYPE.black>
+        {title === 'Swap' ? (
+          <StyledFlexDiv>
+            <TYPE.black my={2} fontWeight={500}>
+              {title}
+            </TYPE.black>
+            <StyledNavLink id={'limit-orders-drawer-nav-link'} to={'/limit-order'}>
+              Limit Order
+            </StyledNavLink>
+          </StyledFlexDiv>
+        ) : (
+          <StyledFlexDiv>
+            <TYPE.black my={2} fontWeight={500}>
+              {title}
+            </TYPE.black>
+            <StyledNavLink id={`swap-nav-link`} to={'/swap'}>
+              Swap
+            </StyledNavLink>
+          </StyledFlexDiv>
+        )}
+
         {hideSettings || <Settings />}
       </RowBetween>
     </StyledSwapHeader>
